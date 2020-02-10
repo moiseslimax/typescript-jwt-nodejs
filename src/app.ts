066fe4c1cdaad881ee6f4a151797333e45d10a4d
Moisés Maldaner de Lima <moiseslimax@gmail.com>
2020-02-10 14:53:33 -0300

    first commit

---
+++ src/app.ts
@@ -0,0 +1,10 @@
+import express, { Application } from 'express';
+
+const app: Application = express();
+
+
+//settings
+app.set('port', 3000)
+
+
+export default app;
---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import app from './app';
+
+function main(){
+    app.listen('port');
+    console.log('Server running on port 3000')
+}
+
+main();
